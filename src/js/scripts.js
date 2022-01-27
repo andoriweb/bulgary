af8e3ea6aadbaa0be2b316e57c291cd7ece440ea
andoriweb <andoriweb@gmail.com>
2022-01-27 11:23:57 +0200

    section feedback slider

---
+++ src/js/scripts.js
@@ -0,0 +1,14 @@
+/* owl.carousel */
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel();
+});
+
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  items:1,
+  margin:10,
+  nav:true
+})
+
+///////////////////////////////////////
+
